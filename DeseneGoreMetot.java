8bef58f7902f197c6bc708901d6eae80ac94b5ec
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-07-24 11:31:14 +0300

    Update DeseneGoreMetot.java

--- DeseneGoreMetot.java
+++ DeseneGoreMetot.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/*
+ Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. Her çıkarma işlemi sırasında ekrana son değeri yazdırın.
+ Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+*/
+
 public class DeseneGoreMetot {
 
     static int sumSub(int number, int i, int a) {
